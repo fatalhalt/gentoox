ae55aa96acce5d234711badf123eb3400d92d1a3
root <root@gocchin.haxx.dafuq>
2021-11-17 15:30:04 -0600

    fix build-stage3.sh script as upstream gentoo now makes a distinction between openrc and systemd tarballs

--- build-stage3.sh
+++ build-stage3.sh
@@ -9,37 +9,38 @@ if [[ ! -f .catalyst-accept-keywords ]]; then
 fi
 
 if [[ ! -f .catalyst-prep-done ]]; then
-  builddate=$(wget --quiet -O - http://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64/ | sed -nr "s/.*href=\"stage3-amd64-([0-9].*).tar.xz\">.*/\1/p")
-  if [[ ! -f "stage3-amd64-$builddate.tar.xz" ]]; then
-    wget http://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64/stage3-amd64-$builddate.tar.xz
+  builddate=$(wget --quiet -O - http://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64-openrc/ | sed -nr "s/.*href=\"stage3-amd64-openrc-([0-9].*).tar.xz\">.*/\1/p")
+  if [[ ! -f "stage3-amd64-openrc-$builddate.tar.xz" ]]; then
+    wget http://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64-openrc/stage3-amd64-openrc-$builddate.tar.xz
   fi
 
   (git clone https://github.com/gentoo/releng.git; cd releng; patch -p1 < ../0001-releng-gcc-add-graphite-support.patch)
 
   mkdir -p /var/tmp/catalyst/builds/default
-  cp -v "stage3-amd64-$builddate.tar.xz" /var/tmp/catalyst/builds/default/stage3-amd64-latest.tar.xz
+  mkdir -p /var/tmp/catalyst/snapshot_cache/latest
+  cp -v "stage3-amd64-openrc-$builddate.tar.xz" /var/tmp/catalyst/builds/default/stage3-amd64-openrc-latest.tar.xz
   emerge --sync
   catalyst -s latest
   touch .catalyst-prep-done
 fi
 
-cp -v releng/releases/specs/amd64/stage{1,2,3}.spec .
+cp -v releng/releases/specs/amd64/stage{1,2,3}-openrc.spec .
 
-sed -i "s/version_stamp: @TIMESTAMP@/version_stamp: $version_stamp/" stage1.spec
-sed -i "s/snapshot: @TIMESTAMP@/snapshot: latest/" stage1.spec
-sed -i "s#@REPO_DIR@#$(pwd)/releng#g" stage1.spec
-sed -i "s/version_stamp: @TIMESTAMP@/version_stamp: $version_stamp/" stage2.spec
-sed -i "s/snapshot: @TIMESTAMP@/snapshot: latest/" stage2.spec
-sed -i "s/source_subpath: default\/stage1-amd64-@TIMESTAMP@/source_subpath: default\/stage1-amd64-$version_stamp/" stage2.spec
-sed -i "s#@REPO_DIR@#$(pwd)/releng#g" stage2.spec
-sed -i "s/version_stamp: @TIMESTAMP@/version_stamp: $version_stamp/" stage3.spec
-sed -i "s/snapshot: @TIMESTAMP@/snapshot: latest/" stage3.spec
-sed -i "s/source_subpath: default\/stage2-amd64-@TIMESTAMP@/source_subpath: default\/stage2-amd64-$version_stamp/" stage3.spec
-sed -i "s#@REPO_DIR@#$(pwd)/releng#g" stage3.spec
+sed -i "s/version_stamp: openrc-@TIMESTAMP@/version_stamp: $version_stamp/" stage1-openrc.spec
+sed -i "s/snapshot: @TIMESTAMP@/snapshot: latest/" stage1-openrc.spec
+sed -i "s#@REPO_DIR@#$(pwd)/releng#g" stage1-openrc.spec
+sed -i "s/version_stamp: openrc-@TIMESTAMP@/version_stamp: $version_stamp/" stage2-openrc.spec
+sed -i "s/snapshot: @TIMESTAMP@/snapshot: latest/" stage2-openrc.spec
+sed -i "s/source_subpath: default\/stage1-amd64-openrc-@TIMESTAMP@/source_subpath: default\/stage1-amd64-$version_stamp/" stage2-openrc.spec
+sed -i "s#@REPO_DIR@#$(pwd)/releng#g" stage2-openrc.spec
+sed -i "s/version_stamp: openrc-@TIMESTAMP@/version_stamp: $version_stamp/" stage3-openrc.spec
+sed -i "s/snapshot: @TIMESTAMP@/snapshot: latest/" stage3-openrc.spec
+sed -i "s/source_subpath: default\/stage2-amd64-openrc-@TIMESTAMP@/source_subpath: default\/stage2-amd64-$version_stamp/" stage3-openrc.spec
+sed -i "s#@REPO_DIR@#$(pwd)/releng#g" stage3-openrc.spec
 
-catalyst -f stage1.spec
-catalyst -f stage2.spec
-catalyst -f stage3.spec
+catalyst -f stage1-openrc.spec
+catalyst -f stage2-openrc.spec
+catalyst -f stage3-openrc.spec
 
 cp -v /var/tmp/catalyst/builds/default/stage3-amd64-$version_stamp.tar.xz .
 
